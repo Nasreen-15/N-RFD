1293df11beb7b559c43f451595eab077c432ee87
ArkamTola <arkamtola15@gmail.com>
2026-02-20 08:15:36 +0530

    Erp_data file

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-
-
-
 from . import views
 
-
-
 urlpatterns = [
-
     path('', views.index, name='index'),
+    path('erp-data/', views.erp_data, name='erp_data'),
+   path('upload-data/', views.upload_data, name='upload_data'),
 
 ]
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
-
-
-
-# Create your views here.
-
-
+from django.utils import timezone
+print(">>> LOADING apps.pages.views <<<")
 
 def index(request):
-
-
-
-    # Page from the theme
-
     return render(request, 'pages/index.html')
+
+def erp_data(request):
+    current_date = timezone.now().strftime("%d-%b-%y")
+    return render(request, 'pages/erp_data.html', {"current_date": current_date})
+
+def upload_data(request):
+    return render(request, 'pages/upload_data.html')
+
