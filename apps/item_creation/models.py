9062ce1addd66c2d8b264cdcbb00918ccf422fd9
Nasreen Mursal <nasreenmursal15@gmail.com>
2026-03-31 17:35:40 +0530

    Implement Item logic

--- apps/item_creation/models.py
+++ apps/item_creation/models.py
@@ -28,3 +28,49 @@ class ApplyTemplate(models.Model):
     class Meta:
         db_table = "tbl_applytemplate"
         managed = False
+
+# --- Corrected Item Model ---
+
+class Item(models.Model):
+    customer_id = models.CharField(max_length=100, null=True, blank=True) # Added null=True
+    customer_name = models.CharField(max_length=255, null=True, blank=True)
+    last_ecn_no = models.CharField(max_length=100, null=True, blank=True)
+    search_item = models.CharField(max_length=255, null=True, blank=True)
+    no = models.CharField(max_length=100, null=True, blank=True)
+    template_name = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    base_unit_of_measure = models.CharField(max_length=50, null=True, blank=True)
+    shelf_no = models.IntegerField(null=True, blank=True)
+    cell = models.CharField(max_length=100, null=True, blank=True)
+    cell_type = models.CharField(max_length=100, null=True, blank=True)
+    item_category_code = models.CharField(max_length=100, null=True, blank=True)
+    product_group_code = models.CharField(max_length=100, null=True, blank=True)
+    status = models.CharField(max_length=100, null=True, blank=True)
+    fixture_no = models.CharField(max_length=100, null=True, blank=True)
+    no_of_meet = models.CharField(max_length=100, null=True, blank=True)
+    no_of_parts = models.CharField(max_length=100, null=True, blank=True)
+    revision_no = models.IntegerField(null=True, blank=True)
+    cust_vender_code = models.CharField(max_length=100, null=True, blank=True)
+    hsn_sac_code = models.CharField(max_length=100, null=True, blank=True)
+    costing_method = models.CharField(max_length=100, null=True, blank=True)
+    inventory_posting_group = models.CharField(max_length=100, null=True, blank=True)
+    price_profit_calculation = models.CharField(max_length=100, null=True, blank=True)
+    last_modified_date = models.DateField(null=True, blank=True)
+    gen_prod_posting_group = models.CharField(max_length=100, null=True, blank=True)
+    replenishment_system = models.CharField(max_length=100, null=True, blank=True)
+    qc_applicable = models.CharField(max_length=100, null=True, blank=True)
+    manufacturing_policy = models.CharField(max_length=100, null=True, blank=True)
+    assembly_policy = models.CharField(max_length=100, null=True, blank=True)
+    reordering_policy = models.CharField(max_length=100, null=True, blank=True)
+    include_inventory = models.CharField(max_length=100, null=True, blank=True)
+    gst_credit = models.CharField(max_length=100, null=True, blank=True)
+    flushing_method = models.CharField(max_length=100, null=True, blank=True)
+    template_applied = models.CharField(max_length=100, null=True, blank=True)
+    rounding_precision = models.CharField(max_length=100, null=True, blank=True)
+    gst_group_code = models.CharField(max_length=100, null=True, blank=True)
+
+    class Meta:
+        db_table = "tbl_item"
+
+    def __str__(self):
+        return self.customer_name
--- apps/opportunities/models.py
+++ apps/opportunities/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Opportunity(models.Model):
+    projectName = models.CharField(max_length=255, null=True, blank=True)
+    customerName = models.CharField(max_length=255, null=True, blank=True)
+    contactName = models.CharField(max_length=255, null=True, blank=True)
+    contactNo = models.CharField(max_length=20, null=True, blank=True)
+    
+    itemNo = models.CharField(max_length=100, null=True, blank=True)
+    custId = models.CharField(max_length=100, null=True, blank=True)
+
+    estimatedSalesPrice = models.FloatField(default=0)
+    nominatedPrice = models.FloatField(default=0)
+
+    creationDate = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=100, null=True, blank=True)
+
+    remarks = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.projectName or "Opportunity"
